ea8b8eca144072037ee75a35ab25b52e5b4e735e
Ubuntu <ubuntu@ip-172-31-35-99.ap-south-1.compute.internal>
2020-07-20 18:35:41 +0000

    first commit of app2

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require('express')
+const app=express()
+const port=4000
+
+app.get('/product4',(req,res) => res.send('Cost of Green Bell Pepper is $3.99/lb'))
+
+
+app.get('/product5',(req,res) => res.send('Cost of Brocolli is $5.00/ea'))
+
+
+app.listen(port,() => console.log('App is listening on port 4000'))
+
