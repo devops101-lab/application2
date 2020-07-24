2a6ebbcd1787be54c136fbd9a132f99040ab7a82
Ubuntu <ubuntu@ip-172-31-35-99.ap-south-1.compute.internal>
2020-07-24 10:22:30 +0000

    testing application 2 jenkins job

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express')
 const app=express()
 const port=4000
 
-app.get('/product4',(req,res) => res.send('Cost of Green Bell Pepper is $3.99/lb'))
+app.get('/product4',(req,res) => res.send('Cost of Green Bell Pepper is $3.99/lb----------------------'))
 
 
 app.get('/product5',(req,res) => res.send('Cost of Brocolli is $5.00/ea'))
